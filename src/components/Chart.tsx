06022c130666d11ea528511ef9ddbbb08203782e
sh4nnongoh <sh4nnongoh@gmail.com>
2022-09-18 21:26:59 +0800

    design: chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,18 +1,34 @@
 import { FC } from "react";
 import {
-  LineChart, Line, XAxis, YAxis
+  LineChart, Line, XAxis, YAxis, Tooltip, Legend
 } from "recharts";
 import getDaysAgoTimestampInSeconds from "../utils/getDaysAgoTimestampInSeconds";
 const Chart: FC<{dailyTransactionCount: number[]}> = ({ dailyTransactionCount }) => {
-  const data = dailyTransactionCount.map((count, index) => ({
-    x: count,
-    y: getDaysAgoTimestampInSeconds(index)
-  }));
+  const data = dailyTransactionCount.map((count, index) => {
+    const timestamp = getDaysAgoTimestampInSeconds(index);
+    return ({
+      name: new Date(timestamp),
+      count,
+      timestamp
+    });
+  });
   return (
-    <LineChart width={400} height={180} data={data}>
-      <Line type="monotone" dataKey="x" stroke="#8884d8" />
-      <XAxis dataKey="y" />
+    <LineChart
+      width={400}
+      height={180}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <Line type="monotone" dataKey="count" stroke="#8884d8" />
+      <XAxis dataKey="timestamp" />
       <YAxis />
+      <Tooltip />
+      <Legend />
     </LineChart>
   );
 };
